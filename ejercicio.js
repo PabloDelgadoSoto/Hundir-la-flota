e21d8878aefb53a3941a53ca1ef2e9dc0eeee6d0
PabloDelgadoSoto <pablodelsot@gmail.com>
2024-10-15 18:54:08 +0200

    colocacion de barcos buena

--- ejercicio.js
+++ ejercicio.js
@@ -1,5 +1,4 @@
 let table;
-let ocupado = [];
 let prohibir = [];
 let cantidad = 4;
 let longitudes = [2, 3, 4, 3];
@@ -19,7 +18,7 @@ const segundo = {
 }
 
 const dificultad = {
-    '1': [9, 10],
+    '1': [10, 10],
     '2': [16, 40],
     '3': [21, 99]
 };
@@ -46,8 +45,6 @@ const hacerTabla = function () {
     ponerBarcos();
     for (let i = 0; i <= tablero; i++) {
         for (let j = 0; j <= tablero; j++) {
-            //document.getElementById(i + '-' + j).addEventListener('mouseenter', cambiarCursor);
-            //document.getElementById(i + '-' + j).addEventListener('mouseleave', restablecer);
             document.getElementById(i + '-' + j).addEventListener('click', hacerClic);
         }
     }
@@ -56,50 +53,74 @@ const hacerTabla = function () {
 const ponerBarcos = function () {
     prohibir = [];
     for (let i = 0; i < cantidad; i++) {
-        barcos[i][0][0] = calcularEspacio(longitudes[i] - 1);
-        barcos[i][0][1] = false;
         let n = Math.floor(Math.random() * 4);
-        console.log(barcos[i][0][0]);
-        for (let j = 1; j < longitudes[i]; j++) {
-            continuar(i, j, n);
+        for (let j = 0; j < longitudes[i]; j++) {
+                barcos[i][j][0] = colocar();
+                prohibir.push(barcos[i][j][0]);
+            while (calcularEspacio(barcos[i][0][0], barcos[i].length-1)){
+                barcos[i][j][0] = colocar();
+                prohibir.push(barcos[i][j][0]);
+            }
+            
+            if(j > 0){
+                if(!continuar(i, j, n)){
+                    j=-1;
+                }
+                barcos[i][0][1] = false;
+            }
         }
-        console.log('a');
-        prohibir = [];
     }
+    verBarcos(); 
 };
-//[][]elegir barco barco,    [][][]que parte del barco
+//[][]elegir barco,    [][][]que parte del barco
 //impedir que salga negativo
 
 const continuar = function (i, j, n) {
-    let b = barcos[i][j-1][0].split('-');
+    //ubicacion anterior
+    let b = barcos[i][j - 1][0].split('-');
     b[segundo[n][0]] = parseInt(b[segundo[n][0]]) + parseInt(segundo[n][1]);
-    let id = parseInt(b[0]) + '-' + parseInt(b[1]);
+    let id = b[0] + '-' + b[1];
+    if(desbordar(b[0]) || desbordar(b[1]) || prohibir.includes(id)){
+        return false;
+    }
     barcos[i][j][0] = id;
     barcos[i][j][1] = false;
-    console.log(barcos[i][j][0]);
-    
+    prohibir.push(barcos[i][j][0]);
+    return true;
+};
+
+const verBarcos = function(){
+    barcos.forEach(barco => {
+        barco.forEach(posicion => {
+            let ph = document.getElementById(posicion[0]);
+            ph.setAttribute('style', 'background-color:pink')
+        });
+    });
 }
 
-const calcularEspacio = function (l) {
-    let escogido = colocar();
+const calcularEspacio = function (escogido, l) {
     let marca = escogido.split('-');
     let fila = marca[0];
     let columna = marca[1];
-    //contarBarcos();
-    if (reservarFila(fila, l) && reservarColumna(columna, l)) {
-        return escogido;
+    let fT = reservarFila(fila, l);
+    let cT = reservarColumna(columna, l);
+    //si no se hace asi por alguna razon no ejecuta ambas
+    prohibir.push(escogido);
+    if (cT && fT) {
+        return true;
     }
-    calcularEspacio();
+    return false;
 };
 
 const reservarFila = function (fila, l) {
+    fila = parseInt(fila);
     for (i = fila - l; i < fila; i++) {
-        if (ocupado.includes(i + '-' + fila)) {
+        if (prohibir.includes(i + '-' + fila) || desbordar(i)) {
             return false;
         }
     }
-    for (i = fila; i < fila + 1; i++) {
-        if (ocupado.includes(i + '-' + fila)) {
+    for (i = fila + 1; i <= fila + l; i++) {
+        if (prohibir.includes(i + '-' + fila) || desbordar(i)) {
             return false;
         }
     }
@@ -107,47 +128,31 @@ const reservarFila = function (fila, l) {
 };
 
 const reservarColumna = function (columna, l) {
+    columna = parseInt(columna);
     for (i = columna - l; i < columna; i++) {
-        if (ocupado.includes(columna + '-' + i)) {
+        if (prohibir.includes(columna + '-' + i) || desbordar(i)) {
             return false;
         }
     }
-    for (i = columna; i < columna + l; i++) {
-        if (inArray(ocupado, columna + '-' + i)) {
+    for (i = columna + 1; i <= columna + l; i++) {
+        if (prohibir.includes(columna + '-' + i) || desbordar(i)) {
             return false;
         }
     }
     return true;
-};
-
-const contarBarcos = function () {
-    for (let i = 0; i < barcos.length; i++) {
-        for (let j = 0; j < barcos[i].length; j++) {
-            ocupado = barcos[i][j][0];
-        }
-    }
 };
 
 const colocar = function () {
     return Math.floor(Math.random() * tablero) + '-' + Math.floor(Math.random() * tablero);
 }
 
-const cambiarCursor = function () {
-    this.setAttribute('class', 'cursor');
-};
-
-const restablecer = function () {
-    let res = document.getElementsByClassName('cursor');
-    res[0].setAttribute('class', '');
-};
-
 const desbordar = function (num) {
     if (num < 0) {
-        return false;
+        return true;
     } else if (num > tablero) {
-        return false;
+        return true;
     }
-    return true;
+    return false;
 };
 
 const hacerClic = function (event) {
@@ -157,7 +162,7 @@ const hacerClic = function (event) {
 
 const marcarXClic = function (id) {
     let marca = document.getElementById(id);
-    if (inArray(ocupado, id)) {
+    if (inArray(prohibir, id)) {
         perder();
         return;
     }
@@ -181,83 +186,3 @@ const marcarXClic = function (id) {
     ganar();
     visto = [];
 };
-
-const recorrer = function (lugar) {
-    let mostrar = [];
-    let id = lugar.getAttribute('id');
-    let p = id.split('-');
-    let u = parseInt(p[0]);
-    let d = parseInt(p[1]);
-    visto.push(id);
-    for (let i = -1; i < 2; i++) {
-        for (let j = -1; j < 2; j++) {
-            let ph1 = u + i;
-            let ph2 = d + j;
-            let resultado = desbordar(ph1) + '-' + desbordar(ph2);
-            let quitar = false;
-            visto.forEach(cas => {
-                if (cas == resultado) {
-                    quitar = true;
-                }
-            });
-            if (!quitar) {
-                visto.push(resultado);
-                mostrar.push(resultado);
-            }
-        }
-    }
-    return mostrar;
-};
-
-const calcularAlrededor = function (id) {
-    let cont = 0;
-    let vistos = [];
-    let p = id.split('-');
-    let u = parseInt(p[0]);
-    let d = parseInt(p[1]);
-    for (let i = -1; i < 2; i++) {
-        for (let j = -1; j < 2; j++) {
-            let ph1 = u + i;
-            let ph2 = d + j;
-            let resultado = desbordar(ph1) + '-' + desbordar(ph2);
-            if (inArray(ocupado, resultado) && !inArray(vistos, resultado)) {
-                cont++;
-                vistos.push(resultado);
-            }
-        }
-    }
-    return cont;
-};
-
-//i+1 j     i-1 j   i j+1   i j-1
-
-const perder = function () {
-    ocupado.forEach(mina => {
-        let m = document.getElementById(mina);
-        m.setAttribute('style', 'background-color:black');
-        escrito.innerHTML = "Has perdido.";
-        perdido = true;
-    });
-};
-
-const rehacer = function (event) {
-    while (table.firstChild) {
-        table.removeChild(table.firstChild);
-    }
-    escrito.innerHTML = '';
-    perdido = false;
-    hacerTabla();
-};
-
-const inArray = function (arr, e) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == e) return true;
-    }
-    return false;
-};
-
-const ganar = function () {
-    if (clicado.length == 0) {
-        escrito.innerHTML = 'Enhorabuena';
-    }
-}
